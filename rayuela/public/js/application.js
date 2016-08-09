1f469da23f0423ea16f6a753225f1bf16d9480fb
codeamac01 <codeamac01@gmail.com>
2016-08-09 13:05:55 -0500

    se añaden los 100 <td>

--- rayuela/public/js/application.js
+++ rayuela/public/js/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
 	// Tu código va aquí
+  for (var i = 0; i <= 100; i++) {
+    $(".table").find("#Player1").append(( "<td>" ));
+  };
+  for (var i = 0; i <= 100; i++) {
+    $(".table").find("#Player2").append(( "<td>" ));
+  };
 
+  
 });
