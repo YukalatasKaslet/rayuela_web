ed234f3e229b279a2000db55fcc4b999212e233d
codeamac01 <codeamac01@gmail.com>
2016-08-09 18:01:03 -0500

    se crea tablero de rayuela con una meta :3

--- rayuela/public/js/application.js
+++ rayuela/public/js/application.js
@@ -7,5 +7,28 @@ $(document).ready(function() {
     $(".table").find("#Player2").append(( "<td>" ));
   };
 
-  
-});
+/*Código prueba para ver método each */
+
+  $( "td" ).each(function( index ) {
+    if( index === 90 || index === 194){
+      $( this ).addClass( "active-meta" );
+      console.log( index + ": " + $( this ).text() );
+    }
+  });
+
+
+
+/* Así puedo colorear al ganador jajajaja XD */
+/*
+  $("#Player1").find( "td" ).each(function( index ) {
+      $( this ).addClass( "prueba" );
+      console.log( index + ": " + $( this ).text() );
+  });  
+
+  $("#Player2").find( "td" ).each(function( index ) {
+    $( this ).addClass( "prueba2" ); 
+    console.log( index + ": " + $( this ).text() );
+  });
+*/
+
+});/* Document.ready */
