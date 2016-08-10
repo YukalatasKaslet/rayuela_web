3f7414558d895b431b46708d119ad61aaf5f77c9
codeamac01 <codeamac01@gmail.com>
2016-08-10 13:57:18 -0500

    se realizo union muchos a muchos

--- rayuela/app/models/game.rb
+++ rayuela/app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :user_games
+  has_many :names, through: :user_games 
 end
--- rayuela/app/models/user.rb
+++ rayuela/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :user_games
+  has_many :scores, through: :user_games
 end
